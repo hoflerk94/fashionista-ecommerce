cdea88048f797b4286e4a80c076c3c5301acea3a
hoflerk94 <karellehofler@gmail.com>
2017-04-18 01:26:33 -0400

    Add theme files

---
+++ js/fashionista.js
@@ -0,0 +1,11 @@
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+    $('.materialboxed').materialbox();
+    $('select').material_select();
+    $('.collapsible').collapsible();
+
+  });
+
+  
+  
